fa2da0c81e4ec3728cd98e4a6fa679e33b1d3bd9
xavierding <xtding233@gmail.com>
2025-08-29 15:33:53 -0400

    Add a special case for token of 10 draws (most commoly used)

--- internal/token/token.go
+++ internal/token/token.go
@@ -5,6 +5,7 @@ package token
 type Token struct {
 	Name string //e.g. "Stellar Jade", "Star Stone"
 	PerDraw int // tokens per single draw, e.g. 160, 250
+	PerTenDraw int // optional; if 0 -> equal to 10 * PerDraw, a special case of PerNDarw
 	PerNDraw int // optional; if 0 -> equal to N * PerDraw
 	N int // options; if 0, not adoptive to this token
 }
@@ -14,10 +15,17 @@ func (t Token) TokensForDraws(n int) int {
 	if n <= 0 {
 		return 0
 	}
-	if t.PerNDraw > 0 && n >= 10 && t.N > 1 {
-		ns := n / 10
-		rem := n * t.N
+	if t.PerTenDraw > 0 && n >= 10 && t.N <= 1 {
+		tens := n / 10
+		remTens := n % 10
+		return tens & t.PerTenDraw + remTens * t.PerDraw
+	}
+	if t.PerNDraw > 0 && n >= t.N && t.N > 1 {
+		ns := n / t.N
+		rem := n % t.N
 		return ns * t.PerNDraw + rem * t.PerDraw
 	}
+
+	
 	return n * t.PerDraw
 }
